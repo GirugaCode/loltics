23a391ac59075c78b4fd3058f8027a4b839af7cf
Ryan Nguyen <ryan.nguyen@students.makeschool.com>
2019-09-03 14:41:53 -0700

    [init] README & INIT

---
+++ loltactics/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  loltactics
+//
+//  Created by Ryan Nguyen on 9/3/19.
+//  Copyright © 2019 Danh Phu Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
