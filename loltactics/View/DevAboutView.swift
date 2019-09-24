b76d05a372ed7d79fef0caa70013029662d0b8e1
Ryan Nguyen <ryan.nguyen@students.makeschool.com>
2019-09-24 14:21:58 -0700

    [add] Tab bar navigation

---
+++ loltactics/View/DevAboutView.swift
@@ -0,0 +1,34 @@
+//
+//  DevAboutView.swift
+//  loltactics
+//
+//  Created by Ryan Nguyen on 9/24/19.
+//  Copyright © 2019 Danh Phu Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class DevAboutView: UIViewController {
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Hide the navigation bar on the this view controller
+        self.navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Show the navigation bar on other view controllers
+        self.navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+    
+
+
+}
