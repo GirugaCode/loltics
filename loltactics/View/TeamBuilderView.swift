4dfb0990b1280a575be66ce53e1ef4b94232a2e0
timo <tmakhlay2@gmail.com>
2019-09-27 01:14:43 -0700

    finished general layout on team

--- loltactics/View/TeamBuilderView.swift
+++ loltactics/View/TeamBuilderView.swift
@@ -76,6 +76,52 @@ class TeamBuilderView: UIViewController {
     var tableView = UITableView()
     
 
+    // CHAMP SELECTS
+    var selectionsBackground: UIView = {
+        var view = UIView()
+        view.layer.borderWidth = 1
+        view.layer.borderColor = #colorLiteral(red: 0.4937598109, green: 0.3993486464, blue: 0.9166263342, alpha: 1)
+        view.layer.shadowOpacity = 1
+        view.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.26).cgColor
+        view.layer.shadowRadius = 4
+        view.layer.shadowOffset = CGSize(width: 0.0, height: 4.0)
+        view.layer.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        view.layer.cornerRadius = UIScreen.main.bounds.width / 27.6
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    var selectionsStack = UIStackView()
+    
+    var first: UIView = {
+        var view = UIView()
+        view.layer.borderWidth = 1
+        view.layer.borderColor = #colorLiteral(red: 0.4937598109, green: 0.3993486464, blue: 0.9166263342, alpha: 1)
+        view.layer.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        view.layer.cornerRadius = UIScreen.main.bounds.width / 27.6
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    var second: UIView = {
+        var view = UIView()
+        view.layer.borderWidth = 1
+        view.layer.borderColor = #colorLiteral(red: 0.4937598109, green: 0.3993486464, blue: 0.9166263342, alpha: 1)
+        view.layer.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        view.layer.cornerRadius = UIScreen.main.bounds.width / 27.6
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    var third: UIView = {
+        var view = UIView()
+        view.layer.borderWidth = 1
+        view.layer.borderColor = #colorLiteral(red: 0.4937598109, green: 0.3993486464, blue: 0.9166263342, alpha: 1)
+        view.layer.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        view.layer.cornerRadius = UIScreen.main.bounds.width / 27.6
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -97,6 +143,8 @@ class TeamBuilderView: UIViewController {
         view.backgroundColor = #colorLiteral(red: 0.9607108235, green: 0.9608257413, blue: 0.9606716037, alpha: 1)
         setupLayout()
         setupTableView()
+        setupChampSelections()
+        
         makeTabBackground()
 
     }
@@ -172,7 +220,7 @@ class TeamBuilderView: UIViewController {
     }
     
     func setupTableView() {
-        tableView = UITableView(frame: CGRect(x: 0, y: collectionViewScale + collectionViewScale / 5, width: view.bounds.size.width, height: (view.bounds.size.height - ((collectionViewScale + collectionViewScale / 5) + (view.bounds.size.height/8)))))
+        tableView = UITableView(frame: CGRect(x: 15, y: collectionViewScale + collectionViewScale / 5, width: view.bounds.size.width - 30, height: (view.bounds.size.height - ((collectionViewScale + collectionViewScale / 5) + (view.bounds.size.height/8)))))
         // Add to Table View to View
         view.addSubview(tableView)
 
@@ -183,12 +231,37 @@ class TeamBuilderView: UIViewController {
         tableView.dataSource = self
         
         // Table View
-        tableView.backgroundColor = .red
+        tableView.backgroundColor = .clear
 //        view.bringSubviewToFront(tableView)
         
 
 
         tableView.separatorStyle = UITableViewCell.SeparatorStyle.none
+    }
+    
+    func setupChampSelections() {
+        // SELECTIONS
+        view.addSubview(selectionsBackground)
+        NSLayoutConstraint.activate([
+            selectionsBackground.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.06),
+            selectionsBackground.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: view.bounds.size.width/3.5),
+            selectionsBackground.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: (view.bounds.size.width/3.5) * -1),
+            selectionsBackground.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: (view.bounds.size.height/7) * -1),
+            selectionsBackground.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0)
+            ])
+        view.bringSubviewToFront(selectionsBackground)
+        
+        selectionsStack = UIStackView(arrangedSubviews: [first,second,third])
+        selectionsStack.axis = .horizontal
+        
+//        view.addSubview(selectionsStack)
+//        NSLayoutConstraint.activate([
+//            selectionsStack.topAnchor.constraint(equalTo: selectionsBackground.topAnchor),
+//            selectionsStack.leadingAnchor.constraint(equalTo: selectionsBackground.leadingAnchor),
+//            selectionsStack.trailingAnchor.constraint(equalTo: selectionsBackground.trailingAnchor),
+//            selectionsStack.bottomAnchor.constraint(equalTo: selectionsBackground.bottomAnchor),
+//            selectionsStack.centerXAnchor.constraint(equalTo: selectionsBackground.centerXAnchor)
+//            ])
     }
 }
 
@@ -199,14 +272,11 @@ extension TeamBuilderView: UICollectionViewDataSource {
             return 5
         } else if section == 1 {
             print("second")
-
             return 5
         }
         else {
             print("third")
-
             return 4
-        
         }
     }
     
@@ -297,7 +367,7 @@ extension TeamBuilderView: UICollectionViewDelegateFlowLayout {
 extension TeamBuilderView: UITableViewDataSource {
     // Table View Rows
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 2
+        return 1
     }
     
     // Table View Cells
